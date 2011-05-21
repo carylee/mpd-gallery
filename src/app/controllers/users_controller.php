a4d6139a5785fd8c671b84c21ed4c43e183bdf4e
Cary Lee <carylee@gmail.com>
2011-05-21 18:51:12 -0500

    Removed Acl related stuff from users_controller

--- src/app/controllers/users_controller.php
+++ src/app/controllers/users_controller.php
@@ -2,7 +2,6 @@
 class UsersController extends AppController {
 
 	var $name = 'Users';
-  var $components = array('Acl');
 
   function login() {
   }
@@ -10,33 +9,6 @@ class UsersController extends AppController {
   function logout() {
     $this->redirect($this->Auth->logout());
   }
-
-  function createAros() {
-    $aro =& $this->Acl->Aro;
-    
-    //Here's all of our group info in an array we can iterate through
-    $groups = array(
-      0 => array(
-        'alias' => 'admins'
-      ),
-      1 => array(
-        'alias' => 'students'
-      ),
-      2 => array(
-        'alias' => 'visitors'
-      ),
-    );
-    
-    //Iterate and create ARO groups
-    foreach($groups as $data)
-    {
-      $aro->create();
-      
-      //Save data
-      $aro->save($data);
-    }
-  }
-
 
 	function index() {
 		$this->User->recursive = 0;
