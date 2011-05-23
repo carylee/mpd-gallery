4fd76bb9a38db87cd9e537ed514fc900089d30b3
tinading <xiaotianding1.2013@u.northwestern.edu>
2011-05-23 12:59:48 -0500

    managetest checkedin

---
+++ src/app/tests/cases/manage.test.php
@@ -0,0 +1,26 @@
+<?php
+class ManageTest extends CakeWebTestCase{
+function ManageTest(){
+	$this->baseurl =$this->base;
+}
+
+function testManageUsers(){
+$this->get($this->baseurl."/users/manage");
+$this->click('add a new user');
+$this->verifyAddNewUserFormDisplayed();
+$this->addUserTest();
+}
+
+function verifyAddNewUserFormDisplayed(){
+$this->assertField('data[User][username]','');
+$this->assertField('data[User][password]','');
+}
+
+function addUserTest(){
+$this->setField('data[User][username]','hitina');
+$this->setField('data[User][password]','mpd');
+$this->click('Submit');
+
+}
+}
+?>
